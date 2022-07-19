141b4bcaa71ea90daa0af2677d18d52e4f9f358c
mubeen ahmad <mubeenahmad1199@gmail.com>
2022-07-19 19:19:55 +0500

    var

---
+++ 1_variable/1_variable.sh
@@ -0,0 +1,18 @@
+# The name of a variable can contain only letters 
+# (a to z or A to Z), 
+# numbers ( 0 to 9) or 
+# the underscore character ( _).
+
+# Defining Variable
+# Note space are not included 
+var="Mubeen"
+
+# Call Vatiable
+# used $sign for call/print variable
+echo $var
+
+var=09890
+echo $var
+
+var=Mubeen
+echo $var
---
+++ 1_variable/4_types_of_variables.sh
@@ -0,0 +1,16 @@
+# Local Variables 
+
+# − A local variable is a variable that is present 
+#   within the current instance of the shell. 
+#   It is not available to programs that are started by the shell. 
+#   They are set at the command prompt.
+
+# example
+local_var="This is Local Variable"
+echo $local_var
+
+# Environment Variables 
+# − An environment variable is available to any child process of the shell. 
+# Some programs need environment variables in order to function correctly. 
+# Usually, a shell script defines only those environment variables that are needed by the programs that it runs.
+
